edba40be54a20c55a5b59a7b03bb87e965b2aa69
abeel50 <abeelmuhammad50@gmail.com>
2022-06-13 15:58:46 +0500

    still 66%

--- OddOccurrencesInArray.py
+++ OddOccurrencesInArray.py
@@ -1,20 +1,11 @@
-from multiprocessing.spawn import _main
-from pip import main
-
-
 def solution(A):
     # if array has single element 
     if len(A) == 1 or len(A) == A.count(A[0]):
         return A[0]
-    count_dict = {}
-    # iterate through array
-    for i in A:
-        if A.count(i) == 1:
-            return i
-        else:
-            if i not in count_dict.keys():
-                count_dict[i] = A.count(i)
-    for (k,v) in count_dict.items():
-        if v % 2 !=0:
-            return k
-  
+    new_A = list(set(A))
+
+    # iterate through unique array
+    for i in new_A:
+            if A.count(i) % 2 != 0:
+                return i
+print(solution([9,9,9,9,1,1,3,3,7,7,7]))
